e8f521a302e905b7090b92bff7621fe875c845a7
tanvi-tejwani-tt <tanvi.tejwani@drcsystems.com>
2024-12-11 11:53:35 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,9 @@
-# app.py
-# This is a test commit
-def add(a, b):
-    return a + b
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+
+for i in range(a,b+1):
+    for j in range(1,11):
+        print(i,"*",j,"=",i*j)
+    print()
